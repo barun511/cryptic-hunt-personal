89bbf184819d732f45681784e3959150b8d27e4b
Barun Parruck <barun.parruck@gmail.com>
2018-09-19 23:54:59 +0530

    Updates answer model

--- hunt/models.py
+++ hunt/models.py
@@ -21,12 +21,12 @@ def save_user_profile(sender, instance, **kwargs):
 
 # TODO having level_title, but only description, seems a bit funny. I think we should make level_title just title, and level_number just number, but we need to change level_number to number in views as well.
 class Level(models.Model):
-    level_title = models.CharField(max_length=100)
+    level_title = models.CharField(max_length=200)
     description = models.TextField()
     level_number = models.IntegerField(primary_key=True)
-    answer1 = models.CharField(max_length=20)
-    answer2 = models.CharField(max_length=20)
-    answer3 = models.CharField(max_length=20)
+    answer1 = models.CharField(max_length=200)
+    answer2 = models.CharField(max_length=200)
+    answer3 = models.CharField(max_length=200)
     def __str__(self):
         return self.level_title + " (" + str(self.level_number) + ")"
     #image = models.ImageField(upload_to="level/") # TODO get this done when you get back home
@@ -34,7 +34,7 @@ class Submission(models.Model):
     level = models.ForeignKey(Level, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     time_of_submission = models.DateTimeField(auto_now_add=True)
-    submitted_answer = models.CharField(max_length=20) # matches the answer field
+    submitted_answer = models.CharField(max_length=200) # matches the answer field
     accepted = models.BooleanField()
     def __str__(self):
         return str(self.user.username) + " answered : " + str(self.submitted_answer) + " for Level : " + str(self.level.level_number) + "(" + str(self.time_of_submission) + ")"
