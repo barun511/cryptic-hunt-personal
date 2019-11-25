57261e030f573d4cdb3b204fd38f7a5b5fbd0770
Barun Parruck <barun.parruck@gmail.com>
2019-11-25 21:04:16 +0530

    Adds max answer length and sets to 5k

--- hunt/models.py
+++ hunt/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.timezone import now
-
+MAX_TITLE_LENGTH = 5000
+MAX_ANSWER_LENGTH = 5000
 class Profile(models.Model):
         user = models.OneToOneField(User, on_delete=models.CASCADE)
         level = models.IntegerField(default=0)
@@ -22,12 +23,12 @@ def save_user_profile(sender, instance, **kwargs):
 
 # TODO having level_title, but only description, seems a bit funny. I think we should make level_title just title, and level_number just number, but we need to change level_number to number in views as well.
 class Level(models.Model):
-    level_title = models.CharField(max_length=200)
+    level_title = models.CharField(max_length=MAX_TITLE_LENGTH)
     description = models.TextField()
     level_number = models.IntegerField(primary_key=True)
-    answer1 = models.CharField(max_length=200)
-    answer2 = models.CharField(max_length=200)
-    answer3 = models.CharField(max_length=200)
+    answer1 = models.CharField(max_length=MAX_ANSWER_LENGTH)
+    answer2 = models.CharField(max_length=MAX_ANSWER_LENGTH)
+    answer3 = models.CharField(max_length=MAX_ANSWER_LENGTH)
     def __str__(self):
         return self.level_title + " (" + str(self.level_number) + ")"
     #image = models.ImageField(upload_to="level/") # TODO get this done when you get back home
@@ -35,10 +36,10 @@ class Submission(models.Model):
     level = models.ForeignKey(Level, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     time_of_submission = models.DateTimeField(auto_now_add=True)
-    submitted_answer = models.CharField(max_length=200) # matches the answer field
+    submitted_answer = models.CharField(max_length=MAX_ANSWER_LENGTH) # matches the answer field
     accepted = models.BooleanField()
     def __str__(self):
         return str(self.user.username) + " answered : " + str(self.submitted_answer) + " for Level : " + str(self.level.level_number) + "(" + str(self.time_of_submission) + ")"
 
 class AppVariable(models.Model):
-    success_sign_up = models.CharField(max_length=200)
+    success_sign_up = models.CharField(max_length=MAX_ANSWER_LENGTH)
